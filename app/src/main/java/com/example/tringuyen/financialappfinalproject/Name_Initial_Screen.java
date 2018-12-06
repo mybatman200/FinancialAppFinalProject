edf1e5cfd0ac82fc9ab56c2c2320d9874ea2527c
mybatman <mybatman200@gmail.com>
2018-12-05 20:23:00 -0500

    First Commit

---
+++ app/src/main/java/com/example/tringuyen/financialappfinalproject/Name_Initial_Screen.java
@@ -0,0 +1,23 @@
+package com.example.tringuyen.financialappfinalproject;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Name_Initial_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name__initial__screen);
+
+    }
+
+    public void saveOnClickUser(View v){
+
+    }
+
+}
